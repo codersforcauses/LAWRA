6ef24c0a91a077071240691b27df8f9014a6b652
Thai <nthai243@yahoo.com.au>
2019-07-28 03:26:06 +0800

    change font to roboto

--- chatbot/react-simple-chatbot/theme.js
+++ chatbot/react-simple-chatbot/theme.js
@@ -1,11 +1,11 @@
 export default {
-  background: "#f5f8fb",
-  fontFamily: "monospace",
-  headerBgColor: "#40A7F1",
-  headerFontColor: "#fff",
-  headerFontSize: "16px",
-  botBubbleColor: "#40A7F1",
-  botFontColor: "#fff",
-  userBubbleColor: "#fff",
-  userFontColor: "#4a4a4a"
+  background: '#f5f8fb',
+  fontFamily: 'Roboto, sans-serif',
+  headerBgColor: '#40A7F1',
+  headerFontColor: '#fff',
+  headerFontSize: '16px',
+  botBubbleColor: '#40A7F1',
+  botFontColor: '#fff',
+  userBubbleColor: '#fff',
+  userFontColor: '#4a4a4a'
 };
